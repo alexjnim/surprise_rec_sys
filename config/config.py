6bd1ee7fcd5d40e44556b1dafaa88200164cd7ac
Alex Nim <alex.nim@ibm.com>
2021-02-22 13:29:18 +0000

    change config and add comments

--- config/config.py
+++ config/config.py
@@ -7,5 +7,6 @@ ratingsColumn = "rating"
 itemTitleColumn = "title"
 
 # this is for the removeOutliers in data/DataLoader.py
-removeOutlierUsers = True
+# set to False here, as we know that the movielens data has been cleaned, so very unlikely to contain bots
+removeOutlierUsers = False
 outlierStdDev = 3.0
